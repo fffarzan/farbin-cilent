aac38273e004590e0dea73cb2dde1380f4c72956
fffarzan <far.ned.me@gmail.com>
2020-07-29 13:36:12 +0430

    Creating route and template

--- src/app/training/training-routing.module.ts
+++ src/app/training/training-routing.module.ts
@@ -4,10 +4,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { TrainingComponent } from './training.component';
 import { TrainingCourseComponent } from './training-course/training-course.component';
 import { TrainingCourseResolver } from './training-course/training-course.resolver';
+import { TrainingCourseHeldBatchListComponent } from './training-course-held-batch-list/training-course-held-batch-list.component';
 
 const routes: Routes = [
   { path: '', component: TrainingComponent },
-  { path: 'training-course/:id', component: TrainingCourseComponent, resolve: { course: TrainingCourseResolver } }
+  { path: 'training-course/:id', component: TrainingCourseComponent, resolve: { course: TrainingCourseResolver } },
+  { path: 'training-course-held-batch-list', component: TrainingCourseHeldBatchListComponent }
 ]
 
 @NgModule({
