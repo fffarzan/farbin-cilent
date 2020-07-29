ae8079486d5622be3f07cada2d555fd2804f8edf
fffarzan <far.ned.me@gmail.com>
2020-07-29 09:18:48 +0430

    Creating the component

---
+++ src/app/training/training-course-held-batch-list/training-course-held-batch-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-training-course-held-batch-list',
+  templateUrl: './training-course-held-batch-list.component.html',
+  styleUrls: ['./training-course-held-batch-list.component.css']
+})
+export class TrainingCourseHeldBatchListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/training/training.module.ts
+++ src/app/training/training.module.ts
@@ -4,11 +4,13 @@ import { TrainingRoutingModule } from './training-routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { TrainingComponent } from './training.component';
 import { TrainingCourseComponent } from './training-course/training-course.component';
+import { TrainingCourseHeldBatchListComponent } from './training-course-held-batch-list/training-course-held-batch-list.component';
 
 @NgModule({
   declarations: [
     TrainingComponent,
-    TrainingCourseComponent
+    TrainingCourseComponent,
+    TrainingCourseHeldBatchListComponent
   ],
   imports: [
     TrainingRoutingModule,
