28d49e77ce871c9b45cf800848b7e39619e8e150
fffarzan <far.ned.me@gmail.com>
2020-07-29 09:17:42 +0430

    Creating the component

---
+++ src/app/training/training-course-held-list/training-course-held-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-training-course-held-list',
+  templateUrl: './training-course-held-list.component.html',
+  styleUrls: ['./training-course-held-list.component.css']
+})
+export class TrainingCourseHeldListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
