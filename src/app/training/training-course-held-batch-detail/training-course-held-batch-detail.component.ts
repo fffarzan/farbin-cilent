9817154880b3c54fa60e1059343d2e31c8973bb9
fffarzan <far.ned.me@gmail.com>
2020-08-01 10:43:33 +0430

    Creating routing and template

---
+++ src/app/training/training-course-held-batch-detail/training-course-held-batch-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-training-course-held-batch-detail',
+  templateUrl: './training-course-held-batch-detail.component.html',
+  styleUrls: ['./training-course-held-batch-detail.component.css']
+})
+export class TrainingCourseHeldBatchDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
