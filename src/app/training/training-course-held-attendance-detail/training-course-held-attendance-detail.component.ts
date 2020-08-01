6622ff5d30e8788c8e29d84c54732f1ac0d128fc
fffarzan <far.ned.me@gmail.com>
2020-08-01 21:55:48 +0430

    Creating routing and template of training-course-held-attendance

---
+++ src/app/training/training-course-held-attendance-detail/training-course-held-attendance-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-training-course-held-attendance-detail',
+  templateUrl: './training-course-held-attendance-detail.component.html',
+  styleUrls: ['./training-course-held-attendance-detail.component.css']
+})
+export class TrainingCourseHeldAttendanceDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/training/training-routing.module.ts
+++ src/app/training/training-routing.module.ts
@@ -7,6 +7,7 @@ import { TrainingCourseHeldBatchListComponent } from './training-course-held-bat
 import { TrainingCourseHeldListComponent } from './training-course-held-list/training-course-held-list.component';
 import { TrainingCourseHeldBatchDetailComponent } from './training-course-held-batch-detail/training-course-held-batch-detail.component';
 import { TrainingCourseHeldDetailComponent } from './training-course-held-detail/training-course-held-detail.component';
+import { TrainingCourseHeldAttendanceDetailComponent } from './training-course-held-attendance-detail/training-course-held-attendance-detail.component';
 
 const routes: Routes = [
   { path: '', component: TrainingComponent },
@@ -14,6 +15,7 @@ const routes: Routes = [
   { path: 'training-course-held-batch/:id', component: TrainingCourseHeldBatchDetailComponent },
   { path: 'training-course-held-batch-list', component: TrainingCourseHeldBatchListComponent },
   { path: 'training-course-held/:id', component: TrainingCourseHeldDetailComponent },
+  { path: 'training-course-held-attendance/:id', component: TrainingCourseHeldAttendanceDetailComponent },
   { path: ':moreCoursesUrl/:id', component: TrainingCourseHeldListComponent }
 ]
 
