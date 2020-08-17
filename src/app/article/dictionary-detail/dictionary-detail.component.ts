4587fa777f99b9d0529f550b8d4af867616297e2
fffarzan <far.ned.me@gmail.com>
2020-08-17 18:07:36 +0430

    Creating the component compeletly

--- src/app/article/dictionary-detail/dictionary-detail.component.ts
+++ src/app/article/dictionary-detail/dictionary-detail.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+
+import { environment } from 'src/environments/environment';
+import { ExtensionMethodService } from 'src/app/shared/extension-method.service';
+import { ArtcileDataStorageService } from '../shared/article-data-storage.service';
+import { DictionaryWord } from './dictionary-word.model';
+import { DictionaryDetailService } from './dictionary-detial.service';
 
 @Component({
   selector: 'app-dictionary-detail',
@@ -6,10 +14,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dictionary-detail.component.css']
 })
 export class DictionaryDetailComponent implements OnInit {
+  enviornment: { production: boolean, baseUrl: string } = environment;
+  isMobile: boolean = this.extensionMethodService.DetectMobile();
+  isTablet: boolean = this.extensionMethodService.DetectTablet();
+  word: DictionaryWord;
+  wordId: number;
 
-  constructor() { }
+  constructor(
+    private dataStorageService: ArtcileDataStorageService,
+    private dictionaryDetailService: DictionaryDetailService,
+    private route: ActivatedRoute,
+    private extensionMethodService: ExtensionMethodService,
+    private location: Location
+  ) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe(param => {
+      this.getDictionaryWordDetailData(+param['id']);
+      this.wordId = +param['id'];
+    });
   }
 
+  onGoBack() {
+    this.location.back();
+  }
+
+  private getDictionaryWordDetailData(id: number) {
+    this.dataStorageService.fetchDictionaryWordDetail({ IDX: id })
+      .subscribe(() => this.word = this.dictionaryDetailService.getDictionaryWordDetail());
+  }
 }
