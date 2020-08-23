186e704a3e1fcbc8d8ce26659c41f439148b7683
fffarzan <far.ned.me@gmail.com>
2020-08-23 14:25:39 +0430

    Creating fixed product menu component

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -5,12 +5,15 @@ import { ProductRouting } from './product.routing';
 import { ProductCategoryComponent } from './product-category/product-category.component';
 import { MasterProductComponent } from './master-product/master-product.component';
 import { DefineDetailProductComponent } from './define-detail-product/define-detail-product.component';
+import { BreadcrumbComponent } from './shared/breadcrumb/breadcrumb.component';
+import { FixedProductMenuComponent } from './shared/fixed-product-menu/fixed-product-menu.component';
 
 @NgModule({
   declarations: [
     ProductCategoryComponent, 
     MasterProductComponent, 
-    DefineDetailProductComponent
+    DefineDetailProductComponent, 
+    BreadcrumbComponent, FixedProductMenuComponent
   ],
   imports: [
     ProductRouting,
---
+++ src/app/product/shared/fixed-product-menu/fixed-product-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fixed-product-menu',
+  templateUrl: './fixed-product-menu.component.html',
+  styleUrls: ['./fixed-product-menu.component.css']
+})
+export class FixedProductMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
