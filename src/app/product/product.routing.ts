b17faa11691dfbb787aac546db09896831154977
fffarzan <far.ned.me@gmail.com>
2020-09-05 17:35:34 +0430

    Implementing template and routing

--- src/app/product/product.routing.ts
+++ src/app/product/product.routing.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { ProductCategoryComponent } from './product-category/product-category.component';
 import { MasterProductComponent } from './master-product/master-product.component';
+import { DefineDetailProductComponent } from './define-detail-product/define-detail-product.component';
 
 const routes: Routes = [
   { path: '', component: ProductCategoryComponent },
-  { path: 'product/master-product/:id', component: MasterProductComponent }
+  { path: 'product/master-product/:id', component: MasterProductComponent },
+  { path: 'product/define-detail-product/:id', component: DefineDetailProductComponent }
 ]
 
 @NgModule({
