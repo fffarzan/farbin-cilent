9c428b2e4799ceb293c75684589e1ad0bdbecbfb
fffarzan <far.ned.me@gmail.com>
2020-09-12 09:18:55 +0430

    Creating compare-list route

--- src/app/product/product.routing.ts
+++ src/app/product/product.routing.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductCategoryComponent } from './product-category/product-category.component';
 import { MasterProductComponent } from './master-product/master-product.component';
 import { DefineDetailProductComponent } from './define-detail-product/define-detail-product.component';
+import { CompareListComponent } from './compare/compare-list/compare-list.component';
 
 const routes: Routes = [
   { path: '', component: ProductCategoryComponent },
   { path: 'product/master-product/:id', component: MasterProductComponent },
-  { path: 'product/define-detail-product/:id', component: DefineDetailProductComponent }
+  { path: 'product/define-detail-product/:id', component: DefineDetailProductComponent },
+  { path: 'product/compare-list', component: CompareListComponent }
 ]
 
 @NgModule({
