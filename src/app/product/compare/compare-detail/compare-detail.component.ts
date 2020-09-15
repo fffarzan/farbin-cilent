5c0812d25d0aea9c64baa1577eaee0aa0c714889
fffarzan <far.ned.me@gmail.com>
2020-09-15 17:30:49 +0430

    Creating core of compare-detail component

--- src/app/product/compare/compare-detail/compare-detail.component.ts
+++ src/app/product/compare/compare-detail/compare-detail.component.ts
@@ -1,15 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { environment } from 'src/environments/environment';
+import { ProductDataStorageService } from '../../shared/product-data-storage.service';
+import { CompareDetailService } from './compare-detail.service';
+import { ProductProperties } from './product-properties.model';
+import { Subscription } from 'rxjs';
+import { DataManagementService } from 'src/app/shared/services/data-management.service';
 
 @Component({
   selector: 'app-compare-detail',
   templateUrl: './compare-detail.component.html',
   styleUrls: ['./compare-detail.component.css']
 })
-export class CompareDetailComponent implements OnInit {
+export class CompareDetailComponent implements OnInit, OnDestroy {
+  enviornment: { production: boolean, baseUrl: string } = environment;
+  props: ProductProperties[];
+  technicalProps: ProductProperties[];
+  propsSub: Subscription;
+  technicalPropsSub: Subscription;
+  routeSub: Subscription;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private dataManagementService: DataManagementService,
+    private dataStorageService: ProductDataStorageService,
+    private compareDetailService: CompareDetailService
+  ) { }
 
   ngOnInit(): void {
+    this.routeSub = this.route.params.subscribe(param => {
+      this.getProductPropertiesData(param['id'])
+      this.getTechnicalProductPropertiesData(param['id'])
+    });
   }
 
+  onAddToMaterialList(id: string) {
+    this.dataManagementService.addToMaterialList(id);
+  }
+
+  ngOnDestroy() {
+    this.routeSub.unsubscribe();
+    this.propsSub.unsubscribe();
+    this.technicalPropsSub.unsubscribe();
+  }
+
+  private getProductPropertiesData(ids: string) {
+    this.propsSub = this.dataStorageService.fetchCompareProductsProperties({ CompareList: ids })
+      .subscribe(() => this.props = this.compareDetailService.getProductProperties());
+  }
+
+  private getTechnicalProductPropertiesData(ids: string) {
+    this.technicalPropsSub = this.dataStorageService.fetchCompareProductsTechnicalProperties({ CompareList: ids })
+      .subscribe(() => this.technicalProps = this.compareDetailService.getProductTechnicalProperties());
+  }
 }
