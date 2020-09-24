5593371dae8a1432aa0be8ad51123cf952afb069
Farzan <far.ned.me@gmail.com>
2020-09-24 10:51:16 +0330

    Change base url to default

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  // baseUrl: 'http://farbin.ir/',
-  baseUrl: "http://192.168.10.15:2015/"
+  baseUrl: 'http://farbin.ir/',
+  // baseUrl: "http://192.168.10.15:2015/"
 };
 
 /*
